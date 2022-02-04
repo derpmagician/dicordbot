0f5f5a6b702f61a14d75f9983c3a47f4a8d27c2d
Renzo Perez <renzin@gmail.com>
2022-02-03 19:04:35 -0500

    updating commands

--- src/index.js
+++ src/index.js
@@ -14,9 +14,17 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('message', (msg) => {
-	if (msg.content === '!ping') {
-		msg.reply('Pong')
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
 	}
 });
 
