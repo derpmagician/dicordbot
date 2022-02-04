dc4f77bdb6514825fe27f92b1104852bb50201a5
Renzo Perez <renzin@gmail.com>
2022-02-03 19:00:25 -0500

    fixing dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-const { Client } = require('discord');
-const client = new Client();
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
+
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+// const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
 
 client.on('message', (msg) => {
-	if (msg.content === '$ping') {
+	if (msg.content === '!ping') {
 		msg.reply('Pong')
 	}
 });
 
-
-client.login(process.env.DISCORD_BOT_KEY);
+// Login to Discord with your client's token
+client.login(process.env.DISCORD_TOKEN);
